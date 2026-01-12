a20e812b74baaef1cda468695f5e01e5b6142db1
Dany <dany@gmail.com>
2026-01-12 09:06:41 -0600

    Done: Menu Mobil

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -68,14 +68,14 @@ const Hero = () => {
             <header className='hero'>
                 <section className='hero-content'>
                     <nav className={`p-3 flex justify-between items-center fixed w-full transition-colors duration-300 ${scrolled
-                            ? 'bg-secondary'
-                            : 'bg-[#b0a884af]'
+                        ? 'bg-secondary'
+                        : 'bg-[#b0a884af]'
                         }`}>
                         <div className='flex gap-3'>
                             <Image src={LogoIcon} width={50} height={50} alt='Logo' />
                             <div className='flex flex-col justify-center items-center uppercase'>
-                                <p className='tracking-widest text-[#f5f2f0e8]'>Grupo</p>
-                                <p className='font-bold text-[#f5f2f0e8]'>Dany Rivera</p>
+                                <p className='tracking-widest text-tertiary'>Grupo</p>
+                                <p className='font-bold text-tertiary'>Dany Rivera</p>
                             </div>
                         </div>
                         <div onClick={handleOpenMenu}>
@@ -122,53 +122,44 @@ const Hero = () => {
             <div>ok</div>
 
             {menuIsAmount && (
-                <section className={`absolute z-30 h-full w-full top-0 bg-[#2f1d0e] px-7 py-10 ${showMenuMobil ? 'menu-visible' : 'menu-hidden'}`}>
+                <section className={`fixed z-30 h-full w-full top-0 text-tertiary bg-secondary px-7 py-10 ${showMenuMobil ? 'menu-visible' : 'menu-hidden'}`}>
                     <div className="flex justify-between items-center">
-                        <div className="flex items-center gap-4">
-                            <Image
-                                src="/images/coffe-header-mobil.svg"
-                                alt="Logo"
-                                width={40}
-                                height={40}
-                            />
-                            <h1 className="text-white tracking-wider m-0 text-2xl font-bold">Dany Coffe</h1>
+                        <div className='flex flex-col justify-start items-start uppercase'>
+                            <p className='tracking-widest  text-xl'>Grupo</p>
+                            <p className='font-bold text-xl'>Dany Rivera</p>
                         </div>
-                        <div>
-                            <svg onClick={handleCloseMenu} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10 text-white">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        <div onClick={handleCloseMenu}>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="text-tertiary size-6" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
                             </svg>
+
                         </div>
                     </div>
 
                     <div className="flex flex-col justify-between h-full py-10">
                         <div>
                             <ul className="flex flex-col gap-5">
-                                <li className="text-xl text-white font-bold tracking-wide border-b-2 pb-3">Oferta</li>
-                                <li className="text-xl text-white font-bold tracking-wide border-b-2 pb-3">Nosotros</li>
-                                <li className="text-xl text-white font-bold tracking-wide border-b-2 pb-3">Reseñas</li>
-                                <li className="text-xl text-white font-bold tracking-wide border-b-2 pb-3">Por que Café Dany</li>
+                                <li className="text-lg  tracking-wider uppercase">Experiencia</li>
+                                <li className="text-lg  tracking-wider uppercase">Nosotros</li>
+                                <li className="text-lg  tracking-wider uppercase">Servicios</li>
+                                <li className="text-lg  tracking-wider uppercase">Clientes</li>
+                                <li className="text-lg  tracking-wider uppercase">Contacto</li>
                             </ul>
                         </div>
 
-                        <div className="flex flex-col gap-5">
-                            <div className="flex gap-5">
-                                <div>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10 text-white">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-                                    </svg>
-                                </div>
-                                <div>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10 text-white">
+                        <div className="">
+                            <div className="flex items-center gap-5">
+                                <div className="relative">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 absolute bottom-3 left-3">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                                     </svg>
+                                    <input className="outline-none border border-tertiary pl-10 pr-3 py-3 text-base rounded-full" type="search" placeholder="Buscar..." />
                                 </div>
-                            </div>
-                            <div className="flex gap-5">
-                                <button className="text-white tracking-wider">Login</button>
-                                <button className="text-white tracking-wider">Sign Up</button>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+                                </svg>
                             </div>
                         </div>
-
                     </div>
                 </section>
             )}

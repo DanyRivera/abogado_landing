7bd162fdf229f5479199a21546f3d89d8cef0462
Dany <dany@gmail.com>
2026-01-18 22:54:13 -0600

    Done with footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,16 +4,16 @@ import LogoIcon from '../../public/logo-abogado.svg';
 const Footer = () => {
     return (
         <footer className='bg-primary mt-20 pt-12'>
-            <div className='flex justify-between items-center mx-10 md:mx-20 mb-10 gap-5 '>
+            <div className='flex justify-between items-center mx-8 md:mx-20 mb-10 gap-5 '>
                 <div className='flex flex-col items-center md:flex-row md:gap-2'>
-                    <Image src={LogoIcon} width={50} height={50} alt='Logo' />
-                    <p className='tracking-widest text-tertiary text-md uppercase mt-2 md:mt-0'>Grupo</p>
-                    <p className='font-bold text-tertiary text-md uppercase'>Dany Rivera</p>
+                    <Image src={LogoIcon} width={40} height={40} alt='Logo' />
+                    <p className='tracking-widest text-tertiary text-sm md:text-md uppercase mt-2 md:mt-0'>Grupo</p>
+                    <p className='font-bold text-tertiary text-sm md:text-md uppercase'>Dany Rivera</p>
                 </div>
                 <div className='md:flex md:gap-5'>
-                    <p className='font-semibold text-xl uppercase text-tertiary mb-4 md:mb-0'>Menu</p>
+                    <p className='font-semibold text-lg uppercase text-tertiary mb-4 md:mb-0'>Menu</p>
 
-                    <ul className='text-tertiary grid grid-cols-2 justify-center items-center gap-3 md:flex'>
+                    <ul className='text-tertiary grid grid-cols-2 justify-center items-center gap-3 md:flex text-sm md:text-base'>
                         <li>Nosotros</li>
                         <li>Casos</li>
                         <li>Servicios</li>
